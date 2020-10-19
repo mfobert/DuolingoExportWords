8cc84a107b36217421451a19e3f67eba6ccc8597
Mark Fobert <markfobert@gmail.com>
2020-10-19 14:43:56 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+You can use the python script below with the downloaded vocabulary overview json file and gets the translation via google translations!
+
+# This is a sample Python script.
+import json
+from googletrans import Translator
+
+#JSON FILE FROM https://www.duolingo.com/vocabulary/overview
+
+def parse_json(filename):
+    #load file
+    file = open(filename, "r")
+
+    json_contents = file.read()
+
+    json_parsed = json.loads(json_contents)
+
+    word_json_objects = json_parsed['vocab_overview']
+    words = []
+    for word_json_object in word_json_objects:
+        words.append(word_json_object['word_string'])
+
+    words.sort()
+    words = list(dict.fromkeys(words)) #remove duplicates
+
+    source_lang = 'es'
+    dest_lang = 'en'
+
+    translator = Translator()
+    translations = translator.translate(words, src=source_lang, dest=dest_lang)
+    for translation in translations:
+
+        print(translation.origin , " , " , translation.text)
+
+
+    file.close()
+
+
+if __name__ == '__main__':
+    parse_json('/home/mark/Downloads/overview.json')
